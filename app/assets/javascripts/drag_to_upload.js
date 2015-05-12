981ad5339b4ce2d8d8f97d4670f9a98da91fd374
James <jaimersonaraujo@gmail.com>
2015-05-12 16:49:15 -0300

    Only show dropzone when dragging files

--- app/assets/javascripts/drag_to_upload.js
+++ app/assets/javascripts/drag_to_upload.js
@@ -1,24 +1,35 @@
 $(document).ready(function(){
   Dropzone.autoDiscover = false;
 
-  var $attachment = $('[data-dropzone]');
-  var $submit = $('#comment_submit');
+  var $attachment_form = $('[data-dropzone]');
+  var dropzoned = false;
 
-  $attachment.dropzone({
-    maxFileSize: 25,
-    paramName: 'ticket[comments_attributes][0][attachment]',
-    autoProcessQueue: false,
-    init: function() {
-      var myDropzone = this;
+  $attachment_form.on('dragover', function(){
+    if(dropzoned) { return };
 
-      // First change the button to actually tell Dropzone to process the queue.
-      this.element.querySelector("input[type=submit]").addEventListener("click", function(e) {
-        // Make sure that the form isn't actually being sent.
-        e.preventDefault();
-        e.stopPropagation();
-        myDropzone.processQueue();
-        setTimeout('location.reload()', 2000);
-      });
-    }
+    $attachment_form.dropzone({
+      maxFileSize: 25,
+      paramName: 'ticket[comments_attributes][0][attachment]',
+      autoProcessQueue: false,
+      init: function() {
+        var dropzone = this;
+
+        // First change the button to actually tell Dropzone to process the queue.
+        this.element.querySelector("input[type=submit]").addEventListener("click", function(e) {
+          // Make sure that the form isn't actually being sent.
+          e.preventDefault();
+          e.stopPropagation();
+          dropzone.processQueue();
+        });
+
+        dropzone.on('success', function(){
+          location.reload();
+        });
+      }
+    });
+
+    dropzoned = true;
+
+    $attachment_form.find('.comment_dropzone').removeClass('hidden');
   });
 });
